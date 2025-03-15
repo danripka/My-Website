0e03d59d639b5a23a6d80db328f9169fa55f60e0
STALWART-3-2\dripk <danripka00@gmail.com>
2025-03-14 22:51:57 -0500

    - Imported html and css files - Added app.py - Added .gitignore

---
+++ app.py
@@ -0,0 +1,27 @@
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return flask.render_template("about.html")
+
+
+@app.route("/projects")
+def projects():
+    return flask.render_template("projects.html")
+
+
+@app.errorhandler(Exception)
+def page_not_found(error):
+    return flask.render_template("error.html", code=error.code, description=error.description), error.code
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
