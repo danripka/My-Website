483d37c5b24bc0009f14874abadde6f4606d2a71
STALWART-3-2\dripk <danripka00@gmail.com>
2025-03-16 16:16:33 -0500

    app.py - Cleaned up errorhandler and added contact page style.css - Added dark theme and Roboto font - Cleaned up html templates - Added contact.html - Added resume - Added favicon.ico

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import flask
+import werkzeug.exceptions
 
 app = flask.Flask(__name__)
 
@@ -8,19 +9,29 @@ def index():
     return flask.render_template("index.html")
 
 
-@app.route("/about")
-def about():
-    return flask.render_template("about.html")
-
-
 @app.route("/projects")
 def projects():
     return flask.render_template("projects.html")
 
 
-@app.errorhandler(Exception)
+@app.route("/about")
+def about():
+    return flask.render_template("about.html")
+
+
+@app.route("/resume")
+def resume():
+    return flask.send_file("documents\\resume.pdf", as_attachment=False)
+
+
+@app.route("/contact")
+def contact():
+    return flask.render_template("contact.html")
+
+
+@app.errorhandler(werkzeug.exceptions.HTTPException)
 def page_not_found(error):
-    return flask.render_template("error.html", code=error.code, description=error.description), error.code
+    return flask.render_template("error.html", error_code=error.code, error_name=error.name), error.code
 
 
 if __name__ == "__main__":
